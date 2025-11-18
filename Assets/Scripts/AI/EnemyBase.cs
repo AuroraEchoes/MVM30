3993948ea3e2db85edf9fd7221e98f3b3ae8bde7
B-DU-DE <thebairdboyMatthew@gmail.com>
2025-11-19 02:08:05 +1100

    Improved "patrol" movement
    Implemented into the base AI class for patrolling
    -To do: implement logic so that AI can agro on player

--- Assets/Scripts/AI/EnemyBase.cs
+++ Assets/Scripts/AI/EnemyBase.cs
@@ -1,23 +1,80 @@
 using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using UnityEngine.Tilemaps;
+using System.Collections;
+using System.Collections.Generic;
 
 public class EnemyBase : MonoBehaviour
 {
     [SerializeField] private string name;
     [SerializeField] public EnemyStats stats;
-    [SerializeField] private AttackBase[] attacks;
+    [SerializeField] protected AttackBase[] attacks;
+    [SerializeField] public Transform EnemyFeetPos;
     private float currentHealth;
     private bool CanBeDamaged = true;
     private bool detectsPlayer = false;
+    public bool isAttacking = false;
+    public bool isCooldown = false;
+    public List<Vector3> Waypoints;
+    protected Tilemap tilemap;
+    protected Rigidbody2D rb;
+    protected Vector2 startPos;
+    protected float currentSpeed;
 
     void Start()
     {
         currentHealth = stats.health;
+        rb = GetComponent<Rigidbody2D>();
+        startPos = transform.position;
+        if (tilemap == null)
+        {
+            foreach(Tilemap map in FindObjectsByType<Tilemap>(FindObjectsSortMode.None))
+            {
+                if (map.name == "Ground")
+                {
+                    tilemap = map;
+                }
+            }
+        }
+        currentSpeed = stats.speed;
+    }
+
+    void FixedUpdate()
+    {
+        if (!isCooldown && !isAttacking)
+        {
+            Vector2 direction = new Vector2(0, 0);
+            if (Waypoints.Count > 0)
+            {
+                direction = (Waypoints[0] - EnemyFeetPos.position).normalized;
+                if (Vector3.Distance(EnemyFeetPos.position, Waypoints[0]) < 0.5f)
+                {
+                    Waypoints.RemoveAt(0);
+                    currentSpeed = stats.speed;
+                    isCooldown = true;
+                    Invoke("CoolDownEnd", Random.Range(0.5f, 1.0f));
+                }
+            }
+            else
+            {
+                Move();
+            }
+            rb.linearVelocity = direction * currentSpeed * Time.fixedDeltaTime;
+        }
     }
 
     public virtual void Move()
-    {}
+    {
+        if (!isAttacking && !isCooldown)
+        {
+            GetRandomPos();
+        }
+    }
+    public void CoolDownEnd()
+    {
+        isCooldown = false;
+    }
 
     public virtual void TakeDamage(float damage)
     {
@@ -33,9 +90,47 @@ public class EnemyBase : MonoBehaviour
             attacks[chosenAttack].DoAttack(this);
         }
     }
+    private void GetRandomPos()
+    {
+        if (tilemap == null) return;
+        Waypoints = new List<Vector3>();
+        Vector3Int randomPos = new Vector3Int(100000000,100000000,100000000);
+        while (!tilemap.cellBounds.Contains(randomPos))
+        {
+            randomPos = GenerateWaypoint();
+            if (tilemap.cellBounds.Contains(randomPos))
+            {
+                if (tilemap.GetTile(randomPos) != null)
+                {
+                    if (tilemap.GetTile(randomPos).name.Contains("wall"))
+                    {
+                        randomPos.x = 1000000000;
+                    }
+                }
+            }
+        }
+        Waypoints.Add(tilemap.GetCellCenterWorld(randomPos));
+    }
 
+    private Vector3Int GenerateWaypoint()
+    {
+        float randomX = startPos.x + Random.Range(-stats.patrolRadius, stats.patrolRadius);
+        float randomY = startPos.y + Random.Range(-stats.patrolRadius, stats.patrolRadius);
+        
+        Vector3Int randomPos = tilemap.WorldToCell(new Vector3(randomX, randomY, 0));
+        return randomPos;
+    }
+    
     public virtual void Death()
     {
         
     }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // TODO: add in implementation for when the player enters a trigger radius so the enemy thats chasing them.
+    }
+    
+    
 }
--- Assets/Scripts/AI/TestScripts/EnemyTest.cs
+++ Assets/Scripts/AI/TestScripts/EnemyTest.cs
@@ -1,41 +1,19 @@
 
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.Tilemaps;
+
+
 
 public class EnemyTest : EnemyBase
 {
-    public List<Vector3> Waypoints;
-    public float currentSpeed = 1;
     public Coroutine attackCoroutine;
-    public Tilemap tilemap;
-    private Vector3 startPosition;
 
-    public bool isAttacking = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        startPosition = transform.position;
-        tilemap = FindAnyObjectByType<Tilemap>();
-        currentSpeed = stats.speed;
-        GetRandomPos();
-        //Attack();
-       // Move();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     
     public override void Move()
     {
-        if (!isAttacking)
-            StartCoroutine(MoveAlongPath());
+        base.Move();
     }
-
+    
     public override void TakeDamage(float damage)
     {
         base.TakeDamage(damage);
@@ -46,49 +24,9 @@ public class EnemyTest : EnemyBase
         base.Attack();
     }
 
-    private void GetRandomPos()
-    {
-        Waypoints = new List<Vector3>();
-        float randomX = startPosition.x + Random.Range(-stats.patrolRadius, stats.patrolRadius);
-        float randomY = startPosition.y + Random.Range(-stats.patrolRadius, stats.patrolRadius);
-        Vector3Int randomPos = new Vector3Int((int)Mathf.Floor(randomX), (int)Mathf.Floor(randomY), 0);
-        if(tilemap.cellBounds.Contains(randomPos))
-        {
-            Debug.Log("Position is within bounds");
-            
-        }
-        Waypoints.Add(tilemap.GetCellCenterWorld(randomPos));
-        Move();
-    }
-
     public override void Death()
     {
         
-    }
-
-    private IEnumerator  MoveAlongPath()
-    {
-        while (Waypoints.Count > 0)
-        {
-            float value = 0;
-            
-            Vector3 StartPosition = transform.position;
-            Vector3 nextposition = tilemap.GetCellCenterWorld(new Vector3Int((int)Mathf.Floor(Waypoints[0].x),
-                (int)Mathf.Floor(Waypoints[0].y), (int)Mathf.Floor(Waypoints[0].z)));
-            float requiredTime = Vector3.Distance(StartPosition, nextposition) / currentSpeed;
-            
-            while (value < 1)
-            {
-                value += Time.deltaTime;
-                gameObject.transform.position = Vector3.Lerp(StartPosition, nextposition, value / requiredTime);
-                yield return null;
-            }
-            Waypoints.RemoveAt(0);
-            yield return null;
-        }
-        
-        yield return new WaitForSeconds(Random.Range(0.5f, 5.0f));
-        GetRandomPos();
     }
     
 }
